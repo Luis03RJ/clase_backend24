4c3898188c9c9abe07cb2726fb4f95615159263e
Luis Angel Ramirez Julian <luisangeljulian22@gmail.com>
2024-10-07 12:04:48 -0600

    control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -3,26 +3,39 @@
 const express= require('express');
 
 const app = express();
+
+const usuarios= [
+    {
+        id: 1,
+        nombre:"juan",
+        apellido:"perez",
+        email:"juanperez@gmail.com",
+    },
+    {
+        id: 2,
+        nombre:"carlos",
+        apellido:"perez",
+        email:"juanperez@gmail.com",
+    },
+
+];
+
 //Get obtener informacion 
 app.get("/usuarios", (req, res) => {
-
-    const usuarios= [
-        {
-            id: 1,
-            nombre:"juan",
-            apellido:"perez",
-            email:"juanperez@gmail.com",
-        },
-        {
-            id: 2,
-            nombre:"carlos",
-            apellido:"perez",
-            email:"juanperez@gmail.com",
-        },
-
-    ];
-    res.status(200).send(usuarios);
+    res.status(200).send({usuarios});
 });
+
+//para el cliente es 1 para el servidor es 0 
+app.get("/usuarios/:id",(req,res)=> {
+   // res.status(200).send(usuarios[usuarios.find]);
+   const {id} =req.params;
+   const usuario = usuarios.find((usuario)=> usuario.id== +id); 
+  
+   res.status(200).send(usuario);
+});
+//cambiamos el 0 a usuarios.find 
+
+
 
 
 app.listen(3000, () => {
