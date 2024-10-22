e039b39bd3d24d965a8bfca191ad498a66a5c665
Luis Angel Ramirez Julian <luisangeljulian22@gmail.com>
2024-10-21 18:17:48 -0600

    backend

---
+++ stock_manager/app.js
@@ -0,0 +1,12 @@
+const express= require('express');
+
+const app =express();
+
+app.get('/', (req, res) => {
+    res.send("Hello wordl");
+});
+
+app.listen(3000, () => {
+    console.log('Example app listenining on port 3000');
+});
+
